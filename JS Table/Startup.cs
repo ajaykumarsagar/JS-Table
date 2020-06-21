c3988da794eb78cc7d2c5275ca04a3c43e14f3ba
ajaykumarsagar <jkmrsgr@gmail.com>
2020-06-21 19:46:23 +0530

    Initial Commit

---
+++ JS Table/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JS_Table.Startup))]
+namespace JS_Table
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
